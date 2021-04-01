21f4751ac4ef3ddcac0f463fed82e4322bb82751
Max Robinson <robinsonmax17@gmail.com>
2021-04-01 12:25:36 +0100

    Injects button into google meet

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -3,10 +3,61 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+		var iconContainer
+		var recordButton
+		var recording = false;
+
+		const inject = () => {
+			iconContainer = document.querySelector("div.f0WtFf")
+			if(!iconContainer){
+				setTimeout(() => {
+					inject()
+				},500)
+				return;
+			}
+	
+			recordButton = document.querySelector("div.uD3s5c").cloneNode()
+			recordButton.setAttribute("id", "custom-record-button")
+			recordButton.addEventListener("click", e => {
+				e.preventDefault()
+				recording = !recording
+				if(recording){
+					startRecording()
+				} else {
+					stopRecording()
+				}
+			})
+			console.log(recordButton)
+			iconContainer.prepend(recordButton)
+			setTimeout(() => {
+				renameRecordButton("Record Screen")
+				recordButton.setAttribute("jscontroller","")
+				recordButton.setAttribute("jsaction","")
+			},1000)
+		}
+		inject()
+
+		const renameRecordButton = name => {
+			try{
+				document.querySelector("div#custom-record-button div.YhIwSc").innerText = name
+				document.querySelector("div#custom-record-button div.U26fgb").setAttribute("class", "U26fgb") // Remove all other classes
+				document.querySelector("div#custom-record-button i.google-material-icons").innerText = "camera"
+				return true
+			} catch (e) {
+				return false
+			}
+		}
+
+		const startRecording = () => {
+			console.log("Stop Recording")
+			renameRecordButton("Start Recording")
+		}
+
+		const stopRecording = () => {
+			console.log("Start Recording")
+			renameRecordButton("Stop Recording")
+		}
+
 
 	}
 	}, 10);
